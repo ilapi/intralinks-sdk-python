4641861fdbda7b22811a89d7ce1d882ea57f4bce
Olivier Mangez <OMangez@intralinks.com>
2018-12-16 22:33:54 +0100

    Fix booleans.py

--- intralinks/utils/booleans.py
+++ intralinks/utils/booleans.py
@@ -3,6 +3,5 @@ def convert_to_bool(o, f):
         for e in o:
             convert_to_bool(e, f)
 
-    elif isinstance(o, dict):
-        if f in o and o[f] in {'T', 'F'}:
-            o[f] = o[f] == 'T'
+    elif isinstance(o, dict) and f in o and o[f] in {'T', 'F'}:
+        o[f] = o[f] == 'T'
