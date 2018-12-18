dcf789d874d907833aa48557309c3e12e4703a4e
Olivier Mangez <OMangez@intralinks.com>
2018-12-18 15:08:25 +0100

    Updating version number

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="intralinks",
-    version="0.1.6",
+    version="0.1.7",
     author="Olivier Mangez",
     author_email="olivier.mangez@gmail.com",
     description="A SDK for intralinks.com (for educational purpose only)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ilapi/intralinks-api-python",
+    url="https://github.com/ilapi/intralinks-sdk-python",
     packages=setuptools.find_packages(),
     install_requires=['requests>0.23.4', 'beautifulsoup4>4.4.0'],
     classifiers=[
